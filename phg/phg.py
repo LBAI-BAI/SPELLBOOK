9e1980b6654a4e28cdb92b87e51cdf2e89314b5a
Nurtal <nathan.foulquier.pro@gmail.com>
2025-01-07 12:00:14 +0100

    add phenograph module

--- phg/phg.py
+++ phg/phg.py
@@ -40,9 +40,51 @@ def check_config(arguments):
 
 
 def run(input_file, output_folder):
-    """ """
+    """Run phenograph clustering
+    Create 3 files : clusters.csv, graph.txt and log.txt
+    N.B : Q : ratio of intra-cluster modularity score to inter-cluster modularity score (The Louvain algorithm tries to optimize it)
 
-    print("Tardis")
+    Args:
+        - input_file (str) : path to input data file
+        - output_folder (str) : path to output folder
+
+    """
+
+    # parameters
+    k = 30
+
+    # load data
+    df = pd.read_csv(input_file)
+    if 'LABEL' in list(df.keys()):
+        df = df.drop(columns=['LABEL'])
+    if 'ID' in list(df.keys()):
+        df.index = df['ID']
+        df = df.drop(columns=['ID'])
+
+    # run phenograph
+    communities, graph, Q = phenograph.cluster(df,k=k)
+
+    # save clusters
+    cluster_data = open(f"{output_folder}/clusters.csv", "w")
+    cluster_data.write("ID,CLUSTER\n")
+    cmpt = 0
+    for i in list(df.index):
+        cluster_data.write(f"{i},{communities[cmpt]}\n")
+        cmpt+=1
+    cluster_data.close()
+
+    # save graph
+    graph_file = open(f"{output_folder}/graph.txt", "w")
+    graph_file.write(str(graph))
+    graph_file.close()
+
+    # save logs
+    log_file = open(f"{output_folder}/log.txt", "w")
+    log_file.write(f"k = {k}\n")
+    log_file.write(f"Q = {Q}\n")
+    log_file.close()
+
+    
 
 
 if __name__ == "__main__":
